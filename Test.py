c6431856184b94ce53f312bd071ecdd7a918f692
Jesse Franks <jessevfranks@gmail.com>
2025-11-15 12:03:22 -0500

    Implement forward chaining

---
+++ Test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from jesse_franks_q3 import forward_chain
+
+
+class MyTestCase(unittest.TestCase):
+    def test_basic(self):
+        rules_input = ["A & B => C", "C => D"]
+        facts_input = {"A", "B"}
+        response = set(forward_chain(rules_input, facts_input))
+        answer = {"A", "B", "C", "D"}
+        self.assertEqual(answer, response)
+
+    def test_advanced(self):
+        rules_input = [
+            "A & B => C",
+            "C => D",
+            "D | E => F",  # Added an OR rule
+            "F & G => H",
+            "AX => Z"  # Added a rule to test substring
+        ]
+
+        facts_input = {"A", "B", "E", "G"}
+        response = set(forward_chain(rules_input, facts_input))
+        answer = {"A", "B", "C", "D", "E", "F", "G", "H"}
+        self.assertEqual(answer, response)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ jesse_franks_q3.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+def forward_chain(rules, facts):
+
+    counts = []
+    parsed_rules = []
+
+    inferred = set(facts) # will not preserve order
+    agenda = deque(facts)
+
+    # Assuming that a "simple" algorithm only expects 1 or 0 operators (ie. A & B | C => D is an invalid input),
+    # and that the predicate only contains one inference (ie. A & B => C | D is invalid)
+    for rule_str in rules:
+        conditions_str, inference = rule_str.split(" => ")
+        rule_data = {
+            'inference': inference,
+            'premises': set()
+        }
+
+        if " & " in conditions_str:
+            premises = conditions_str.split(" & ")
+            counts.append(len(premises))
+            rule_data['premises'] = set(p.strip() for p in premises)
+        elif " | " in conditions_str:
+            premises = conditions_str.split(" | ")
+            counts.append(1)
+            rule_data['premises'] = set(p.strip() for p in premises)
+        else:
+            counts.append(1)
+            rule_data['premises'] = {conditions_str.strip()}
+        parsed_rules.append(rule_data)
+
+
+    while agenda:
+        fact = agenda.popleft()
+
+        for i, rule in enumerate(parsed_rules):
+            if counts[i] > 0 and fact in rule['premises']:
+                counts[i] -= 1
+
+            if counts[i] == 0:
+                new_inference = rule['inference']
+                if new_inference not in inferred:
+                    inferred.add(new_inference)
+                    agenda.appendleft(new_inference)
+
+    return inferred
